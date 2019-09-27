8a8252510bbcfa7f0ca248769fbeb6fe2b2a9011
Archelaus <nathanjdawson@gmail.com>
2019-09-27 19:25:23 +0100

    feat: project base and gulp tasks

---
+++ gulpfile.babel.js
@@ -0,0 +1,27 @@
+const { src, dest, series, watch } = require('gulp');
+const babel = require('gulp-babel');
+
+function typescript(){
+    return src('./src/**/*.ts')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(dest('dist'));
+}
+
+function html() {
+    return src('./src/**/*.html')
+        .pipe(dest('dist'));
+}
+
+function monitor() {
+    watch('./src/**/*.ts').on('change', typescript);
+    watch('./src/**/*.html').on('change', html);
+}
+
+module.exports = {
+    typescript: typescript,
+    html: html,
+    default: series(typescript, html, monitor)
+} 
+    
