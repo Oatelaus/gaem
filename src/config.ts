ae3a1c3eef541d0f849b781b629b50d2328dfc8a
Archelaus <nathanjdawson@gmail.com>
2019-09-27 21:58:10 +0100

    chore: added base files

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+import { AUTO } from 'phaser';
+
+export default {
+    type: AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-fuck you
+import config from './config';
+import { Game } from 'phaser';
+
+class Gaem extends Game {
+    constructor() {
+        super(config);
+    }
+}
+
+window.onload = () => {
+    const gaem = new Gaem();
+}
